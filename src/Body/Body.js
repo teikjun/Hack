f845f1df394549dd8abb688248527ec3e15740ab
Kendrick <kendrick.wh@outlook.com>
2020-01-19 02:45:01 +0800

    add bounce animation

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -5,12 +5,16 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
+import { bounce } from 'react-animations';
+import styled, { keyframes } from 'styled-components';
+
+const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} infinite`;
 
 class Body extends React.Component {
     render() {
         return (
             <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100">
-                <img className="emoji" src={emoji} alt=""/>
+                <Bounce><img className="emoji" src={emoji} alt=""/></Bounce>
                 <InputGroup className="m-5">
                     <FormControl
                         placeholder="Name goes here"
