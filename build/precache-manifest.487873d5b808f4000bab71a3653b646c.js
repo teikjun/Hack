cd89aee4d91de6d033250aae9624eddc8d8485aa
JiaYee <lowjiayee123@gmail.com>
2020-01-19 02:26:17 +0800

    Add build

---
+++ build/precache-manifest.487873d5b808f4000bab71a3653b646c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4d352cd21d2c7679a26e7a6bfd16e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d0fb12c5c30a68e4db8",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a3ac2398ad563155d534",
+    "url": "/static/css/main.b5970942.chunk.css"
+  },
+  {
+    "revision": "3d0fb12c5c30a68e4db8",
+    "url": "/static/js/2.175fe5c4.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.175fe5c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3ac2398ad563155d534",
+    "url": "/static/js/main.157f67bc.chunk.js"
+  },
+  {
+    "revision": "8421c3fc5acefc3cb36e",
+    "url": "/static/js/runtime-main.f28035a2.js"
+  },
+  {
+    "revision": "0980aff777838367f6905ddb1eea7878",
+    "url": "/static/media/emoji.0980aff7.svg"
+  }
+]);
