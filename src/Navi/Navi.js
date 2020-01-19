72ca8d9c12d839a25ac54e1695eb923840f38610
Kendrick <kendrick.wh@outlook.com>
2020-01-19 12:07:08 +0800

    blah

--- src/Navi/Navi.js
+++ src/Navi/Navi.js
@@ -8,9 +8,11 @@ class Navi extends Component {
             <div>
                 <Navbar bg="dark" variant="dark" expand="lg">
                     <Navbar.Brand href="/">ANGERY</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
                             <Nav.Link href="/about">About</Nav.Link>
+                            <Nav.Link href="#">Game</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
