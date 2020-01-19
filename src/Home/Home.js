8504683d12e8aa47186ad6a8189d2ef9512ea95d
JiaYee <lowjiayee123@gmail.com>
2020-01-19 11:37:06 +0800

    Show insult

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,56 +7,54 @@ import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 
 class Home extends React.Component {
+    state = {
+        name: "You",
+    };
+
+    handleChange = event => {
+        this.setState({ name: event.target.value });
+    };
+
+    getInsult = () => { 
+        var client = new HttpClient();
+        client.get("https://insult.mattbas.org/api/insult", (response) => {
+            response = " is".concat(response.substr(7));
+            response = this.state.name.concat(response);
+            document.getElementById("response").innerHTML = response;
+        });
+    }
+    
     render() {
         return (
             <div className="container d-flex flex-column justify-content-center align-items-center">
                 <img className="emoji" src={emoji} alt=""/>
                 <InputGroup className="m-3">
-                    <FormControl
+                    <FormControl onChange={this.handleChange}
                         placeholder="Name goes here"
                         aria-label="A hated name"
                     />
                     <InputGroup.Append>
-                        <Button onClick={getInsult} variant="outline-primary">Click me!</Button>
+                        <Button onClick={this.getInsult} variant="outline-primary">Click me!</Button>
                     </InputGroup.Append>
                 </InputGroup>
                 <Alert variant="dark">
-                    <span>Your Mom</span>
+                    <h1 id="response">Prepare some tissues</h1>
                 </Alert>
             </div>
         )
-    }
+    };
 }
 
 export default Home;
 
-function getInsult() {
-    var client = new HttpClient();
-    client.get("https://insult.mattbas.org/api/insult", function(response) {
-        console.log(response);
-    });
-    alert('Hello!');
-}
-
-
 var HttpClient = function() {
     this.get = function(theUrl, callback) {
-        var http_request;
-        http_request = new XMLHttpRequest();
-        http_request.onreadystatechange = function() {
-            if (http_request.readyState === 4 && http_request.status === 200)
-                callback(http_request.responseText);
-        };
-        http_request.open("GET", theUrl);
-        http_request.withCredentials = true;
-        http_request.setRequestHeader("Content-Type", "application/json");
-        http_request.send({ "request": "authentication token" });
-        // var xmlHttp = new XMLHttpRequest();
-        // xmlHttp.onreadystatechange = function() { 
-            // if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
-            //     callback(xmlHttp.responseText);
-        // }
-        // xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-        // xmlHttp.send(null);
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+                callback(xmlHttp.responseText);
+        }
+        xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+        xmlHttp.send(null);
     }
 }
